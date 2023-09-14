58dd3996a5fe21a1db24d89b1ce2bbd1e278eee1
thamizhema <thamizharasan2373@gmail.com>
2023-09-14 08:19:22 +0530

    project created

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+module.exports = app;
